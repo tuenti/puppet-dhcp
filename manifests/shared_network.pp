6d8ebeb7fbf8c344b90203c03b5ce64107c0227c
Victor Cabezas <vcabezas@tuenti.com>
2018-11-12 19:04:45 +0100

    Don't redeclare shared_network concats

--- manifests/shared_network.pp
+++ manifests/shared_network.pp
@@ -7,12 +7,12 @@ define dhcp::shared_network {
   $dhcp_dir = $dhcp::params::dhcp_dir
 
   concat{"${dhcp_dir}/dhcpd.${name}.shared_networks": }
-  concat::fragment { "dhcp_shared_network_${name}":
+  concat::fragment { "dhcp_shared_network_${name}_header":
     target  => "${dhcp_dir}/dhcpd.${name}.shared_networks",
     content => template('dhcp/dhcpd.shared_network.header.erb'),
     order   => '01',
   }
-  concat::fragment { "dhcp_shared_network_${name}":
+  concat::fragment { "dhcp_shared_network_${name}_tail":
     target  => "${dhcp_dir}/dhcpd.${name}.shared_networks",
     content => '}',
     order   => '99',
